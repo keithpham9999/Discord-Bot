4e7d7ab22e4614208f21d4f90615c5d9a54d4313
keithpham9999 <keith.pham.2020@live.rhul.ac.uk>
2023-08-16 14:01:42 +0700

    Add constant variable to proxy

--- proxy.js
+++ proxy.js
@@ -1,3 +1,15 @@
 const axios = require("axios");
 const htmlparser = require("node-html-parser");
 
+const State = {
+    list: [],
+    requests: []
+  };
+  
+  const ProxySites = [
+    "https://sslproxies.org/",
+    "https://free-proxy-list.net/",
+    "https://free-proxy-list.net/anonymous-proxy.html"
+  ];
+  
+  
