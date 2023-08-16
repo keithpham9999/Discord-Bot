1d4c9680d89a6df5efc09958775ca4482324ce75
keithpham9999 <keith.pham.2020@live.rhul.ac.uk>
2023-08-16 13:54:31 +0700

    Add function changeProxy() to test.js and add new file proxy.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-const scrapeJob = require('./scraping')     //This is for running scraping user data from scraping.js
-//const filterBot = require('./test')           //This is for testing only
+//const scrapeJob = require('./scraping')     //This is for running scraping user data from scraping.js
+const filterBot = require('./test')           //This is for testing only
+
 
 
 
--- test.js
+++ test.js
@@ -52,7 +52,30 @@ async function filterBot() {
 }
 
 
-client.on('ready', filterBot)
+
+async function changeProxy() {
+
+    let proxy = await proxyjs.GetRandom();
+    console.log(proxy);
+        // { ip: '127.0.0.1', port: '8080' }
+  
+    // example proxy usage
+    let Request = await axios({
+      url: "https://google.com/",
+      method: "get",
+      headers: { "user-agent": "Mozilla" },
+      proxy: {
+        host: proxy.ip,
+        port: proxy.port
+      }
+    });
+  
+    console.log(Request.data);
+        // request body
+  }
+
+
+client.on('ready', changeProxy)
 
 
 
